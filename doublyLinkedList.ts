f2fee1a49752c57acf3dd8df15b43e17816375cd
luisrevillam <luis.revilla@policygenius.com>
2021-01-30 13:11:07 -0600

    groundwork

---
+++ doublyLinkedList.ts
@@ -0,0 +1,69 @@
+
+interface ListNode {
+  value: any;
+  prev?: ListNode;
+  next?: ListNode;
+}
+
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+interface DoublyLinkedList {
+  head: ListNode;
+  tail: ListNode;
+}
+
+// Feel free to add new properties and methods to the class.
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  setHead(node) {	
+	this.head.prev = node;
+	
+	node.next = this.head;
+    
+	this.head = node;
+  }
+
+  setTail(node) {
+    // to set the tail, we must take the tail, make its next the new node.
+    // make the new nodes prev the tail. 
+    // then change the tail
+    
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+  }
+
+  insertBefore(node, nodeToInsert) {
+    // Write your code here.
+  }
+
+  insertAfter(node, nodeToInsert) {
+    // Write your code here.
+  }
+
+  insertAtPosition(position, nodeToInsert) {
+    // Write your code here.
+  }
+
+  removeNodesWithValue(value) {
+    // Write your code here.
+  }
+
+  remove(node) {
+    // Write your code here.
+  }
+
+  containsNodeWithValue(value) {
+    // Write your code here.
+  }
+}
